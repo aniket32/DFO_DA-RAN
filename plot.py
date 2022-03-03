c7a7db389cd6be89976eef1fc4d2017c3b05c3ac
Aniket <aniketbasu32@gmail.com>
2022-03-03 23:17:23 +0000

    Coordinates and Plot

--- plot.py
+++ plot.py
@@ -6,8 +6,8 @@ import numpy as np
 # plt.rcParams["figure.autolayout"] = True
 
 # Initialising random points for the users
-x = np.arange(50)
-y = np.random.randint(20, size=50)
+x = np.arange(40)
+y = np.random.randint(20, size=40)
 
 # Plot labels and plot type
 plt.scatter(x, y, color='b', marker='^')
@@ -17,10 +17,21 @@ plt.ylabel("y")
 # User Coordinates
 coordinates = []
 for i, j in zip(x, y):
-    coordinates.append([i, j])
+    coordinates.append((i,j))
+
+file = open('coordinates.txt', 'w')
+for items in coordinates:
+    line = ' '.join(str(x) for x in items)
+    file.write(line + '\n')
+
+# for items in coordinates:
+#     print(items)
 
 # Show Plot
-plt.grid(True)
+# plt.grid(True)
+fig1 = plt.gcf()
+plt.savefig('Users.png')
 plt.show()
+plt.draw()
 # Saving the plot
-# plt.savefig("Users.png")
+
